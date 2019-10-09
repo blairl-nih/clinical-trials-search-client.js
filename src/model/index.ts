1dbf913ab785a2becc221ef9f2f1274a317ea670
Blair Learn <blairl-nih@users.noreply.github.com>
2019-10-09 17:15:29 -0400

    Add searchTrials() method. - Define ClinicaltrialResult class. - Define ClinicaltrialResults class. - ClinicalTrialsServiceV1Impl   - Convert getTerms(), getInterventions(), and getDiseases() to async. - CTAPIConnectionV1Impl   - Convert getRequest() to async.   - Add postRequest().

--- src/model/index.ts
+++ src/model/index.ts
@@ -4,3 +4,8 @@ export * from './intervention-result';
 export * from './intervention-results';
 export * from './disease-result';
 export * from './disease-results';
+// NOTE: the export for clinicaltrial-results (plural) **MUST** come before the
+//       one for clinicaltrial-result (singular). Otherwise, there are errors when
+//       an attempt is made to reference the ClinicaltrialResults type.
+export * from './clinicaltrial-results';
+export * from './clinicaltrial-result';
