114de3ce5443b13792ce7c796fd1e7077a9810ab
Blair Learn <blairl-nih@users.noreply.github.com>
2019-10-11 17:26:58 -0400

    Add getTrial() method. - Add optional headers parameter to getRequest/putRequest. - Adjust searchTrials to add "Content-Type: application/json"

--- src/v1/ctapi-connection-v1-impl.ts
+++ src/v1/ctapi-connection-v1-impl.ts
@@ -11,6 +11,12 @@ export class CTAPIConnectionV1Impl implements CTAPIConnection {
   private hostname: string;
   private port: number;
 
+  /**
+   * 
+   * @param protocol Connection protocol (e.g. "https")
+   * @param hostname API hostname (e.g. "clinicaltrials.cancer.gov")
+   * @param port Port number Optional.
+   */
   constructor(protocol: string, hostname: string, port?: number) {
     this.protocol = protocol;
     this.hostname = hostname;
@@ -21,23 +27,26 @@ export class CTAPIConnectionV1Impl implements CTAPIConnection {
     configuration.getConfiguration();
   }
 
-  async getRequest(path: string, params: any): Promise<any> {
+  async getRequest(path: string, params: any, headers?: any): Promise<any> {
     let port: string = this.port ? `:${this.port}` : "";
     let reqURL: string = `${this.protocol}://${this.hostname}${port}/v1${path}`;
 
     const res: AxiosResponse = await axios
       .get(reqURL, {
-        params: params
+        params: params,
+        headers: headers
       });
     return res.data;
   }
 
-  async postRequest(path: string, body: any): Promise<any> {
+  async postRequest(path: string, body: any, headers?: any): Promise<any> {
     let port: string = this.port ? `:${this.port}` : "";
     let reqURL: string = `${this.protocol}://${this.hostname}${port}/v1${path}`;
 
     const res: AxiosResponse = await axios
-      .post(reqURL, body);
+      .post(reqURL, body, {
+        headers: headers
+      });
 
     return res.data;
   }
